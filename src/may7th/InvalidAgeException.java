578b8816638a3e0b9827f340aa6753c5dd092362
seleniumriyaz <selenium.riyaz.shared@gmail.com>
2024-05-07 21:41:40 +0530

    Java Season 2 programs till May 7th

---
+++ src/may7th/InvalidAgeException.java
@@ -0,0 +1,10 @@
+package may7th;
+
+public class InvalidAgeException extends RuntimeException {
+	
+	public InvalidAgeException()
+	{
+		super("Invalid Age ..Please come to this page once your age is 18..");
+	}
+
+}
